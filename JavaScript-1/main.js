54fff7a1aa3fcd8110b31e98ac7a0dec0eeb87d4
nishi-0113 <135202029+nishi-0113@users.noreply.github.com>
2023-07-04 08:16:06 +0900

    1

---
+++ JavaScript-1/main.js
@@ -0,0 +1,9 @@
+function changeBtn() {
+    let p1 = document.getElementById('p1');
+
+    if (p1.style.display === 'block') {
+        p1.style.display = 'none';
+    }else{
+        p1.style.display = 'block';
+    }
+}
